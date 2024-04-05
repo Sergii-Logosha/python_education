0f255580fa8df245b17d83445e8d1268b85fbe23
Sergii Logosha <sergiilogosha@gmail.com>
2024-04-05 13:07:13 +0300

    create Day_1 project band_name_generator

---
+++ 100_days_of_code_dr_angela_yu/auditorium_exercises/day_1_printing/band_name_generator.py
@@ -0,0 +1,6 @@
+# 05/04/2024 Sergii Logosha sergiilogosha@gmail.com
+
+print('Welcome to the Band Name Generator!')
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print('Your band name could be ' + city_name + " " + pet_name)
